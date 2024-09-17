6d1bbdfaf5bd9208f99c4d1d36887e78e4fc8de3
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-09-16 20:01:40 -0700

    added functionality
    Added round ending and restarting, detection of a full house roll and straight roll

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const rulesBtn = document.getElementById("rules-btn");
 let diceValuesArr = [];
 let isModalShowing = false;
 let score = 0;
-let round = 1; 
-let rolls = 0; 
+let round = 1;
+let rolls = 0;
 
 const rollDice = () => {
   diceValuesArr = [];
@@ -83,6 +83,23 @@ const getHighestDuplicates = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const detectFullHouse = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+
+  if (hasThreeOfAKind && hasPair) {
+    updateRadioOption(2, 25);
+  }
+
+  updateRadioOption(5, 0);
+};
+
 const resetRadioOptions = () => {
   scoreInputs.forEach((input) => {
     input.disabled = true;
@@ -94,6 +111,45 @@ const resetRadioOptions = () => {
   });
 };
 
+const resetGame = () => {
+  diceValuesArr = [0, 0, 0, 0, 0];
+  score = 0;
+  round = 1;
+  rolls = 0;
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+
+  totalScoreElement.textContent = score;
+  scoreHistory.innerHTML = "";
+
+  rollsElement.textContent = rolls;
+  roundElement.textContent = round;
+
+  resetRadioOptions();
+};
+
+const checkForStraights = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  const keys = Object.keys(counts).join('');
+
+  if (keys === "12345" || keys === "23456") {
+    updateRadioOption(4, 40);
+  }
+
+  if (keys.slice(0, 4) === "1234" || keys.slice(0, 4) === "2345" || keys.slice(1, 5) === '2345' || keys.slice(1,5) === "2345" || keys === "3456") {
+    updateRadioOption(3, 30);
+  }
+
+  updateRadioOption(5, 0);
+};
+
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
@@ -103,6 +159,8 @@ rollDiceBtn.addEventListener("click", () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
+    detectFullHouse(diceValuesArr);
+    checkForStraights();
   }
 });
 
@@ -136,7 +194,12 @@ keepScoreBtn.addEventListener("click", () => {
     updateStats();
     resetRadioOptions();
     updateScore(selectedValue, achieved);
-    
+    if (round > 6) {
+      setTimeout(() => {
+        alert(`Game Over! Your total score is ${score}`);
+        resetGame();
+      }, 500);
+    }
   } else {
     alert("Please select an option or roll the dice");
   }
